5156bb6f1dbf99971c245248c6539fcbd266efd4
Tsungyu Yu <ytyubox@gmail.com>
2022-06-18 22:37:04 +0800

    init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,24 +1,27 @@
 //
-/* 
+/*
  *		Created by 游宗諭 in 2022/6/18
- *		
+ *
  *		Using Swift 5.0
- *		
+ *
  *		Running on macOS 12.4
  */
 
-
 import SwiftUI
+protocol View_ {
+  associatedtype Body: View_
+  var body: Body { get }
+}
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
