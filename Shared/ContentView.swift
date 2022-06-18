ab5a73286157bef7fe61225b136835fd095921b5
Tsungyu Yu <ytyubox@gmail.com>
2022-06-18 22:36:39 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,24 @@
+//
+/* 
+ *		Created by 游宗諭 in 2022/6/18
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 12.4
+ */
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ViewApp.swift
@@ -0,0 +1,20 @@
+//
+/* 
+ *		Created by 游宗諭 in 2022/6/18
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 12.4
+ */
+
+
+import SwiftUI
+
+@main
+struct ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
